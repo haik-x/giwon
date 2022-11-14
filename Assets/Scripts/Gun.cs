52a022ed31a6b346a0527e156fdd9cfc1164265d
Benjamin Vergara <53795904+haik-x@users.noreply.github.com>
2022-11-13 18:49:04 -0600

    raycasting

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+    public Camera fpsCam;
+    public GameObject impact;
+
+    
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position,fpsCam.transform.forward, out hit,range)){
+            if(hit.transform.CompareTag("Enemy")){
+                GameObject go = Instantiate(impact,hit.point,Quaternion.LookRotation(hit.normal));
+                Debug.Log("You shot poor Slender");
+            }else if(hit.transform.CompareTag("Wall")){
+                Debug.Log("You shot the wall");
+            }
+            }
+        }
+
+}
